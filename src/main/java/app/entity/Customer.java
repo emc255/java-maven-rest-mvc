2cf1e43503d760fd484fdb65a06e73d8d36c6043
emc <micha3l_255@yahoo.com>
2023-06-05 09:39:47 -0500

    add database

--- src/main/java/app/entity/Customer.java
+++ src/main/java/app/entity/Customer.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -17,8 +19,10 @@ import java.util.UUID;
 public class Customer {
     @Id
     @GeneratedValue(generator = "UUID")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.CHAR)
     private UUID id;
+
     @Version
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer version;
